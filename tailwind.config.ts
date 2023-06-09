d0791898de2652bfcf8e7e63efd88291c83d8402
aeases <96164686+Aeases@users.noreply.github.com1~>
2023-06-09 15:39:49 +0800

    Update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'lobster': ['Poppins', 'sans-serif']
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
