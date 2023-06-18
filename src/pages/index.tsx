9a540ae0715da973aa984ced55a76ad82b452efc
Aeases <96164686+Aeases@users.noreply.github.com>
2023-06-18 21:18:52 +0800

    Redid Loading Logic again, removed some uneccessary comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,16 +6,13 @@ import { ProductDisplayComponent } from "~/components/ProductDisplayComponent";
 import { NavItem } from "~/components/NavItem";
 import { ProductComponent } from "~/components/ProductComponent";
 import { Footer } from "~/components/Footer";
-/* <div className="w-[80%] relative h-[25vh] mt-[-12.5vh] bg-slate-300"> {/*Take Height Value, divide by 2 to get mt- value*/
-//fff
-//</div> */
 import { Carousel, Spinner } from 'flowbite-react';
 import Link from "next/link";
 import { useState } from "react";
 
 
 const Home: NextPage = () => {
-  const [Loading_Video, setLoading] = useState(true)
+  const [Loading_Video, setLoading] = useState(false)
 
   return (
     <>
@@ -24,18 +21,19 @@ const Home: NextPage = () => {
         <meta name="description" content="Any Icecream, Any Occasion" />
         <link rel="icon" href="/pictures/conno-favicon.png" />
       </Head>
-      <main className="flex w-full flex-col items-center">
-      {Loading_Video && 
-  <div className="bg-black flex justify-center items-center transition-all opacity-100 ease-in-out duration-1000 h-screen" id="page-loader">
+      {Loading_Video && (
+  <div className="flex justify-center items-center h-screen bg-black" id="page-loader">
   <div className="absolute top-1/2 left-1/2 -ml-24 -mt-3">
-    <div className="rounded-lg shadow-lg p-2 text-3xl animate-pulse flex items-baseline"><p>Loading...</p></div>
+    <div className="rounded-lg shadow-inset p-2 text-3xl animate-pulse flex items-baseline bg-zinc-900 text-white"><p>Loading...</p></div>
     <Spinner color="info" className="mx-auto w-full mt-2 h-12"/>
   </div>
-</div>}
+</div>)}
+      <main className="flex w-full flex-col items-center">
+
         <div className="overflow-hidden w-full h-auto bg-gray-900 -z-20">
           <div className="relative">
           {Loading_Video && (<Spinner color="failure" id="Spinner" className="absolute top-[20%] overflow-hidden left-[25%] h-[50%] w-[50%] "/>)}
-            <video onLoadedData={() => setLoading(false)} className="object-cover object-center relative h-[50vh] w-full left-0 right-0 top-[-75%] -z-10" loop={true} autoPlay={true} muted={true} disablePictureInPicture controls={false} src="/videos/product.mp4"></video>
+            <video onLoadStart={() => setLoading(true)} onCanPlay={() => setLoading(false)} className="object-cover object-center relative h-[50vh] w-full left-0 right-0 top-[-75%] -z-10" loop={true} autoPlay={true} muted={true} disablePictureInPicture controls={false} src="/videos/product.mp4"></video>
           {!Loading_Video && <img src="/pictures/Connoisseur-Logo.webp" className="absolute w-full left-0 right-0 top-[5%] lg:top-[-10%] scale-75 lg:scale-50"></img>}
           </div>
         </div>
@@ -63,17 +61,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-
-
-
-
-
-
-{/* <div className="m-2 bg-slate-200 w-[85%] h-20 flex flex-row justify-center">
-<div className="font-sans justify-center w-max h-full text-center items-center">Local Produce</div>
-<div className="my-4 mx-3 border-slate-100 border-[0.1rem] text-slate-100 rounded-lg w-full flex items-center justify-center justify-items-center"><p className="mb-[0.15rem]">Corporate</p></div>
-<div className="my-4 mx-2 bg-slate-600 w-full flex items-center justify-center"><p>fff</p></div>
-<div className="my-4 mx-2 bg-slate-600 w-full flex items-center justify-center"><p>fff</p></div>
-<div className="my-4 mx-3 border-slate-100 border-[0.1rem] text-slate-100 rounded-lg w-full flex items-center justify-center justify-items-center flex-shrink"><p className="mb-[0.15rem]">Login</p></div>
-</div> */}
