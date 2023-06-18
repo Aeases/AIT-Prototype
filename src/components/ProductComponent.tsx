338849208cc5ef5a4a515d7a26beb0e1b5a05467
Aeases <96164686+Aeases@users.noreply.github.com>
2023-06-18 18:41:08 +0800

    Fixed issue with SVG & added loading spinner to ProductComponent

--- src/components/ProductComponent.tsx
+++ src/components/ProductComponent.tsx
@@ -1,14 +1,20 @@
+import { Spinner } from "flowbite-react"
 import Image from "next/image"
 import Link from "next/link"
+import { useState } from "react"
 
 export function ProductComponent({source, displayText, description_text, path}: {source: string, displayText: string, description_text: string, path: string}) {
     if (!path) {path = "/404"}
+    const [Loading_Picture, setLoading] = useState(true)
+    
     return(
         <div className="">
         <Link href={`${path}`}>
         <div className="flex flex-col p-1 rounded-2xl shadow-md hover:shadow-xl transition-all duration-200 group">
-        <div className="overflow-visible ">
-          <Image src={source} alt={displayText} width="300" height="500"  className="mx-auto h-56 object-cover rounded-xl brightness-90 transition-all duration-500 group-hover:brightness-95 overflow-clip group-hover:scale-105"></Image>
+        <div className="overflow-visible relative">
+          {Loading_Picture && (<Spinner color="success" id="Spinner" className="absolute top-1/4 left-1/4 h-[50%] w-[50%] "/>)}
+          <Image onLoad={() => setLoading(false)} src={source} alt={displayText} width="300" height="500"  className="mx-auto h-56 object-cover rounded-xl brightness-90 transition-all duration-500 group-hover:brightness-95 overflow-clip group-hover:scale-105"></Image>
+          
         </div>
         <div className="mr-auto mt-1 ml-2 w-full">
           <p className="text-2xl font-sans font-bold text-center">{displayText}</p>
--- src/pages/purchase/[slug].tsx
+++ src/pages/purchase/[slug].tsx
@@ -62,7 +62,7 @@ export default function Page() {
     <div className="h-[100vh] overflow-hidden md:max-w-[85%] max-w-none mx-auto">
     <div className="flex flex-row w-full items-center p-3">
         <button onClick={() => void router.push("/Products")} type="button" className="transition-shadow flip-svg-horizontal border hover:shadow-lg  font-medium rounded-lg text-sm p-2.5 text-center inline-flex items-center mr-2 ">
-            <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd" ></path></svg>
+            <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" ></path></svg>
             <span className="sr-only">Icon description</span>
         </button>
     <span className="align-middle justify-center text-center font-lobster w-full text-3xl" >{Current_Selected_Icecream}</span>
