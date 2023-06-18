91a2af669410eafbd23d3105c85cde268381bea5
Aeases <96164686+Aeases@users.noreply.github.com>
2023-06-18 17:58:03 +0800

    Updated Products page to list out icecreams from icecreams.json, now adding an icecream to icecream.json should populate icecreams everywhere on the site.

--- src/components/ProductComponent.tsx
+++ src/components/ProductComponent.tsx
@@ -1,19 +1,22 @@
 import Image from "next/image"
+import Link from "next/link"
 
-export function ProductComponent({source, displayText, description_text}: {source: string, displayText: string, description_text: string}) {
-    const ImageClassName = `w-full brightness-90 transition-all duration-500 group-hover:brightness-95 group-hover:scale-110`
+export function ProductComponent({source, displayText, description_text, path}: {source: string, displayText: string, description_text: string, path: string}) {
+    if (!path) {path = "/404"}
     return(
         <div className="">
+        <Link href={`${path}`}>
         <div className="flex flex-col p-1 rounded-2xl shadow-md hover:shadow-xl transition-all duration-200 group">
-        <div className="max-w-sm overflow-hidden rounded-xl ">
-          <Image src={source} alt={displayText} width="300" height="500"  className={ImageClassName}></Image>
+        <div className="overflow-visible ">
+          <Image src={source} alt={displayText} width="300" height="500"  className="mx-auto h-56 object-cover rounded-xl brightness-90 transition-all duration-500 group-hover:brightness-95 overflow-clip group-hover:scale-105"></Image>
         </div>
         <div className="mr-auto mt-1 ml-2 w-full">
           <p className="text-2xl font-sans font-bold text-center">{displayText}</p>
           <div className="w-full h-[0.01rem] bg-gray-300 my-2 "></div>
-          <p className="font-mono mt-2">{description_text}</p>
+          <p className="font-mono mt-2 w-3/4 mx-auto">{description_text}</p>
         </div>
       </div>
+        </Link>
         </div>
     )
 }
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -2,19 +2,25 @@ import { Footer } from "~/components/Footer";
 import Header from "~/components/Header";
 import { NavItem } from "~/components/NavItem";
 import { ProductComponent } from "~/components/ProductComponent";
-
+import icecreams from "public/icecreams.json"
+interface Iicecream {
+  IceCreamSlug: string,
+  Current_Selected_Price: number,
+  Current_Selected_Icecream_Picture: string,
+  Current_Selected_Icecream: string
+}
 
 export default function Products() {
+  let typedIcecreams = icecreams as unknown as Iicecream[]
+
+  
   return (
     <>
     <Header /> 
-      <div className="grid grid-cols-3 gap-4 mt-6 px-4">
-        <ProductComponent description_text="delcious icecream best icecream cant get better" displayText="Caramel" source="/pictures/conno-tub.webp" />
-        <ProductComponent description_text="delcious icecream best icecream cant get better" displayText="Caramel" source="/pictures/conno-tub.webp" />
-        <ProductComponent description_text="delcious icecream best icecream cant get better" displayText="Caramel" source="/pictures/conno-tub.webp" />
-        <ProductComponent description_text="delcious icecream best icecream cant get better" displayText="Caramel" source="/pictures/conno-tub.webp" />
-        <ProductComponent description_text="delcious icecream best icecream cant get better" displayText="Caramel" source="/pictures/conno-tub.webp" />
-        <ProductComponent description_text="delcious icecream best icecream cant get better" displayText="Caramel" source="/pictures/conno-tub.webp" />
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-6 px-4">
+          {typedIcecreams.map((e) => {
+            return(<ProductComponent path={`/purchase/${e.IceCreamSlug}`} description_text="delcious icecream best icecream cant get better" displayText={e.Current_Selected_Icecream} source={e.Current_Selected_Icecream_Picture} />)
+          })}
       </div>
       <Footer />
     </>
