e54e89b13fde0457f1b9caba8f3de187257a2bc8
Aeases <96164686+Aeases@users.noreply.github.com>
2023-06-18 18:42:41 +0800

    Added Sitemap to Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,23 +6,38 @@ export function Footer() {
     return(
       <div className="w-full bg-stone-500 border-t-stone-400 border-spacing-3 s-2 border-t-4 h-96 mt-24">
         <div className="m-6 grid grid-cols-2">
-            <div className="justify-center w-full font-sans ml-12 flex flex-col gap-8">
+            <div className="justify-start w-full font-sans ml-12 flex flex-col gap-8">
                 <FooterItem text="About"/>
                 <FooterItem text="Corporate"/>
                 <FooterItem text="Contact Info"/>
                 {/* <FontAwesomeIcon icon="twitter"/> */}
             </div>
-          <div className="justify-center w-full font-sans flex flex-col">
-            <a className="text-slate-200 w-full">fff</a>
-            <a className="text-slate-200 underline w-full">ffff</a>
+          <div className="justify-start w-full font-sans flex flex-col gap-3">
+            <p className="text-white text-lg font-bold">Prototype Site Map</p>
+                <FooterItem path="/" text="Home Page"/>
+                <FooterItem path="/Products" text="Products Page"/>
+                <div className="tooltip">
+                  <FooterItem path="/purchase/Coookies&Cream" text="Purchase Page (Coookies & Cream)"/>
+                  <span className="tooltiptext">Purchase Page uses a Dynamic Route, this link only goes to the Cookies & Cream varient of this page, for more see <p className='font-bold text-green-400nm'>product page</p></span>
+                </div>
+                <FooterItem path="/ShopFinder" text="ShopFinder Page"/>
+
+
           </div>
         </div>
       </div>
     )
 }
 
-function FooterItem({text}: {text: string}) {
+function FooterItem({text, path}: {text: string, path? : string}) {
+  if (path) {
     return(
-        <a className="text-gray-200 underline-offset-4 underline hover:underline-offset-1 transition-all">{text}</a>
+      <a href={`${path}`} className="text-gray-200 underline-offset-4 underline hover:underline-offset-1 transition-all">{text}</a>
+  )
+  } else {
+    return(
+      <a className="text-gray-200 underline-offset-4 underline hover:underline-offset-1 transition-all">{text}</a>
     )
+  }
+
 }
